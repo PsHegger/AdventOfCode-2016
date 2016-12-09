c38687172e5033ed9dc3bb936fc893008ead5704
gergelyhegedus <pshegger@gmail.com>
2016-12-09 12:16:37 +0100

    Added solution for day 9, part 2

--- day09/src/main.hs
+++ day09/src/main.hs
@@ -3,7 +3,7 @@ import Data.List.Split (wordsBy)
 import Data.List (intercalate)
 
 decompress :: String -> String
-decompress "" = ""
+decompress ""       = ""
 decompress ('(':ls) = current ++ (decompress next)
     where
         marker      = takeWhile (/=')') ls
@@ -14,11 +14,27 @@ decompress ('(':ls) = current ++ (decompress next)
         code        = take c remaining
         current     = intercalate "" (replicate n code)
         next        = drop c remaining
-decompress (l:ls) = [l] ++ (decompress ls)
+decompress (l:ls)   = [l] ++ (decompress ls)
+
+decompressedLength :: String -> Int
+decompressedLength " "      = 0
+decompressedLength "\n"     = 0
+decompressedLength ""       = 0
+decompressedLength ('(':ls) = (n * (decompressedLength current)) + (decompressedLength next)
+    where
+        marker      = takeWhile (/=')') ls
+        splitMarker = wordsBy (=='x') marker
+        c           = (read (splitMarker !! 0)) :: Int
+        n           = (read (splitMarker !! 1)) :: Int
+        remaining   = drop (length marker + 1) ls
+        current     = take c remaining
+        next        = drop c remaining
+decompressedLength (l:ls) = 1 + (decompressedLength ls)
 
 solve :: String -> Integer -> Int
 solve s 1 = length solution
     where solution = filter (\x -> x /= ' ' && x /= '\n') (decompress s)
+solve s 2 = decompressedLength s
 solve _ _ = error "Unsupported task number"
 
 main = do
